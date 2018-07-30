67bf31718c525bb46d76cea57a9742349b44e1c5
923132714 <923132714@qq.com>
2018-07-30 13:12:31 +0800

    连接成功

--- 数据库/PyMysql.py
+++ 数据库/PyMysql.py
@@ -1,21 +1,28 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
+#coding=utf-8
 
 import pymysql
 
-# 打开数据库连接
-db = pymysql.connect(host='47.93.185.183',user='python',passwd='lalala427',db='python',port=3306,charset='utf8')
-
-'''
-# 使用 cursor() 方法创建一个游标对象 cursor
-cursor = db.cursor()
-
-# 使用 execute()  方法执行 SQL 查询 
-cursor.execute("SELECT VERSION()")
-
-# 使用 fetchone() 方法获取单条数据.
-data = cursor.fetchone()
-
-print ("Database version : %s " % data)
-'''
-# 关闭数据库连接
-db.close()
+#创建数据库连接，注意这里我加入了charset和cursorclass参数
+conn = pymysql.connect(
+    host = "lunareclipse.net.cn",
+    user = "python",
+    password = "lalala427",
+    database = "python",
+    charset = 'utf8',
+    cursorclass = pymysql.cursors.DictCursor)
+#获取游标
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM Test;")
+#fetchall：获取所有的数据，默认以元祖的方式返回，如果你指定了cursorclass = pymysql.cursors.DictCursor，则以dict的方式返回
+#result = cursor.fetchall()
+#for row in result:
+#    print("id是%d,name是%s，age是%d"%(row["id"],row["name"],row["age"]))
+#fetchone：获取剩余数据的第一条数据
+result = cursor.fetchone()
+print(result)
+#fetchmany:获取剩余数据的前2条数据
+#result = cursor.fetchmany(2)
+#print(result)
+cursor.close()
+conn.close()
